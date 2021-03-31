f4af96507fd9c6ceb9cb8bf645e6f8312e4d1029
unknown <rupachasgvn@gmail.com>
2021-03-31 22:11:36 +0545

    modified .gitignore

--- frontend/core/pages/index.tsx
+++ frontend/core/pages/index.tsx
@@ -1,13 +1,10 @@
 import Head from 'next/head'
-import Test from '../components/test'
-
 
 export default function Home() {
   return (
     <div>
       <p className = "text-red-500">
-        Hello Next 
-        <Test/>
+        Hello Next
       </p>
     </div>
   )
