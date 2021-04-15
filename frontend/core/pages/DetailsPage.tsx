2fdd73f92a20bc20c52977ec0a49e9f291f9d9d6
karkigrishmin <grishmink@gmail.com>
2021-04-15 12:55:17 +0545

    created this page to show product details

---
+++ frontend/core/pages/DetailsPage.tsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import ProductDescription from "../src/components/product_details/ProductDescription";
+
+// img(icon)
+// title
+//tagline
+// category
+
+const Details: React.FC = () => {
+	const initialValue = {
+		title: "Instagram App",
+		tagline: "The way to share your photo to the world .",
+		category: ["IOS", "Android", "Website"],
+		img: "./images/snapchat.png",
+	};
+
+	const [data, setData] = useState(initialValue);
+
+	return (
+		// container for the details
+		<div className="bg-drak_blue_background h-auto min-h-screen p-8">
+			{/* product name with icon */}
+			<div className="text-gray-100 flex space-x-5 items-center">
+				<div>
+					<img src={data.img} alt="icon" width="50" />
+				</div>
+
+				<div className="space-y-1">
+					<h1 className="">{data.title}</h1>
+					<p className="text-sm text-gray-300">{data.tagline}</p>
+					<div className="flex space-x-3 text-xs ">
+						{data.category.map((item) => (
+							<div className="cursor-pointer border-gray-300 border px-2 py-1">
+								{item}
+							</div>
+						))}
+					</div>
+				</div>
+			</div>
+
+			{/* product description with screenshot */}
+			<ProductDescription />
+
+			{/* Product discussion component */}
+			{/* <div className="text-gray-100 mt-8">
+				<h1 className="uppercase text-xs py-6">Discussion</h1>
+				<ProductDiscussion />
+			</div> */}
+		</div>
+	);
+};
+
+export default Details;
