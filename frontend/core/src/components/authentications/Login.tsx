f3ce0bbf4293e1703484eb40e0a3d1837798d35f
unknown <rupachasgvn@gmail.com>
2021-04-26 17:47:46 +0545

    passowrd regex validate

--- frontend/core/src/components/authentications/Login.tsx
+++ frontend/core/src/components/authentications/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect , useContext} from 'react';
+import React, { useState, useEffect , useContext, useRef} from 'react';
 import { connect } from 'react-redux';
 import { login } from '../../actions/auth.action';
 import { useRouter } from 'next/router'
@@ -19,16 +19,21 @@ const Login: React.FC = React.memo(({ access, error, isAuthenticated, login, set
 
   const [fromData, setFormData] = useState(initialValues);
   const { email, password } = fromData;
+  const inputRef = useRef([])
 
  
   const onSubmit = async (e) => {
     e.preventDefault();
-    for (const key in FormData)
+    
+    for (let i in inputRef.current)
     {
-      if (fromData[key] == '')
+      if (inputRef.current[i].value == '')
       {
+        inputRef.current[i].classList.add('red-border')
+        inputRef.current[i].placeholder = "Can't be empty !"
+        inputRef.current[i].focus()
         return;
-      }
+      }  
     }
 
     const res = await login(email, password)
@@ -66,8 +71,8 @@ const Login: React.FC = React.memo(({ access, error, isAuthenticated, login, set
       <div className="w-full h-auto">
         <div><img src="/images/default_profileimg.jpg"  className="w-32 h-32 rounded-full m-auto"></img></div>
         <form onSubmit={onSubmit}>
-          <input type="email" onBlur={ onBlur} name ="email" value={email} required placeholder="Email" className="block" onChange={onChange}></input>
-          <input type="password" onBlur={onBlur} name="password" value={password} required placeholder="Password" className="block" onChange={onChange}></input>
+          <input type="email" ref={ref => {inputRef.current[0] = ref}} onBlur={ onBlur} name ="email" value={email} required placeholder="Email" className="block" onChange={onChange}></input>
+          <input type="password" ref={ref => {inputRef.current[1] = ref}} onBlur={onBlur} name="password" value={password} required placeholder="Password" className="block" onChange={onChange}></input>
           {error != null && <h3 className="text-red-700"> Something went wrong !</h3> }
           <button type="button" className="w-full h-12 bg-color5 text-white" onClick={onSubmit}>Login</button>
           <h2>No account yet ?</h2>
