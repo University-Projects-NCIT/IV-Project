2f1c422742dce59487ccbaeae8462283c5fdeb4a
karkigrishmin <grishmink@gmail.com>
2021-05-04 19:01:56 +0545

    working with api

--- frontend/core/pages/DetailsPage.tsx
+++ frontend/core/pages/DetailsPage.tsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { useQuery } from "react-query";
 import ProductComment from "../src/components/product_details/ProductComment";
 import ProductDescription from "../src/components/product_details/ProductDescription";
+import { getProduct } from "../src/productapi";
 
 // img(icon)
 // title
@@ -17,48 +19,48 @@ const Details: React.FC = () => {
 
 	const [data, setData] = useState(initialValue);
 
+	const queryData = useQuery("product details", getProduct);
+	console.log("reactquery data", queryData);
+
 	return (
 		// container for the details
 		<div className="bg-drak_blue_background h-auto min-h-screen p-8 flex">
 			{/* left side container */}
 			<div>
-          {/* product name with icon */}
-			<div className="text-gray-100 flex space-x-5 items-center">
-				<div>
-					<img src={data.img} alt="icon" className="w-20 h-20" />
-				</div>
+				{/* product name with icon */}
+				<div className="text-gray-100 flex space-x-5 items-center">
+					<div>
+						<img src={data.img} alt="icon" className="w-20 h-20" />
+					</div>
 
-				<div className="space-y-1">
-					<h1 className="">{data.title}</h1>
-					<p className="text-sm text-gray-300">{data.tagline}</p>
-					<div className="flex space-x-3 text-xs ">
-						{data.category.map((item) => (
-							<div className="cursor-pointer border-gray-300 border px-2 py-1">
-								{item}
-							</div>
-						))}
+					<div className="space-y-1">
+						<h1 className="">{data.title}</h1>
+						<p className="text-sm text-gray-300">{data.tagline}</p>
+						<div className="flex space-x-3 text-xs ">
+							{data.category.map((item) => (
+								<div className="cursor-pointer border-gray-300 border px-2 py-1">
+									{item}
+								</div>
+							))}
+						</div>
 					</div>
 				</div>
-			</div>
 
-			{/* product description with screenshot */}
-			<ProductDescription />
+				{/* product description with screenshot */}
+				<ProductDescription />
 
-			{/* Product discussion component */}
-			{/* <div className="text-gray-100 mt-8">
+				{/* Product discussion component */}
+				{/* <div className="text-gray-100 mt-8">
 				<h1 className="uppercase text-xs py-6">Discussion</h1>
 				<ProductDiscussion />
 			</div> */}
 
-			{/* Comment component */}
-			<div className="mt-8 text-white ">
-				<h3 className="uppercase text-xs py-5">Comment</h3>
-				<ProductComment />
+				{/* Comment component */}
+				<div className="mt-8 text-white ">
+					<h3 className="uppercase text-xs py-5">Comment</h3>
+					<ProductComment />
+				</div>
 			</div>
-			</div>
-
-		
-      			
 		</div>
 	);
 };
--- frontend/core/src/productapi.ts
+++ frontend/core/src/productapi.ts
@@ -1,23 +1,23 @@
-import axios from 'axios'
+import axios from "axios";
 
-const API_URL = "http://localhost:9000"
+const API_URL = "http://localhost:9000";
 
-const access = (typeof window !== "undefined") ? localStorage.getItem('access') : '';
+// const access =
+// 	typeof window !== "undefined" ? localStorage.getItem("access") : "";
 
 const config = {
-    headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-    }
+	headers: {
+		"Content-Type": "application/json",
+		Accept: "application/json",
+	},
 };
 
-export const fecthProductsByNewest = async (key) => {
-  try {
-    const res = await axios.get(`${API_URL}/products/`, config);
-    console.log(res + " response data");
-    return res;
-  } catch (err)
-  {
-    console.log(err)
-  }
-}
+export const getProduct = async () => {
+	try {
+		const res = await axios.get(`${API_URL}/products/`, config);
+		console.log(res, "get products");
+		return res;
+	} catch (e) {
+		console.log(e);
+	}
+};
