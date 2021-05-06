b47584f5c3995e958d01db8565ce662fe0a927ec
karkigrishmin <grishmink@gmail.com>
2021-05-06 12:20:28 +0545

    changes done by jivan

--- frontend/core/src/components/MainContent.tsx
+++ frontend/core/src/components/MainContent.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect ,useState} from "react";
+import React, { useEffect, useState } from "react";
 import ProductListCard from "./ProductListCard";
 import { FiMoreHorizontal } from "react-icons/fi";
 import { MdAdd } from "react-icons/md";
@@ -8,14 +8,14 @@ import Search from "./Search";
 import UpcomingProductCard from "./UpcomingProductCard";
 import NewsLetterCard from "./NewsLetterCard";
 import { useToggle } from "../hooks/Toggle";
-import AuthForm from './authentications/Auth';
-import { useRouter } from 'next/router';
-import { ToggleContext } from '../Contexts/ToggleContext';
-import { connect } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid'
-import { fecthProducts } from '../productapi';
-import { useQuery } from 'react-query'
-import {NEWEST, POPULAR} from '../constraints'
+import AuthForm from "./authentications/Auth";
+import { useRouter } from "next/router";
+import { ToggleContext } from "../Contexts/ToggleContext";
+import { connect } from "react-redux";
+import { v4 as uuidv4 } from "uuid";
+import { fecthProducts } from "../productapi";
+import { useQuery } from "react-query";
+import { NEWEST, POPULAR } from "../constraints";
 
 const MainContent: React.FC = React.memo(
 	({ user, isAuthenticated }: any): JSX.Element => {
@@ -27,95 +27,83 @@ const MainContent: React.FC = React.memo(
 
 		//Pop us when click to profile image if loged in is false
 		const [loginForm, toggle] = useToggle(false);
+		const [productOrderBy, setProductOrderBy] = useState(NEWEST);
 		const router = useRouter();
+		let launchedData, upcommingData;
 
-		// 	const { data, error, isLoading, isError } = useQuery("products", fecthProductsByNewest);
-		// console.log(data + " datafrom api p")
+		const { data, error, isLoading, isError } = useQuery(
+			"products",
+			fecthProducts
+		);
 
-		const initialData = [
-			[
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-			],
+		if (isError || error) {
+			return <h1>Error occurs </h1>;
+		}
 
-			[
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-			],
+		if (isLoading) {
+			return <h1>Data is loading </h1>;
+		}
 
-			[
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-				{
-					title: "Instagram App",
-					date: "2020 march 13",
-					tagline: "The way to share your photo to the world .",
-					category: ["IOS", "Android", "Wesite"],
-				},
-			],
-		];
+		const filterLaunched = (data) => {
+			/**
+			 * This function filters the data according to the
+			 * Date it is launched . Only already lauhced data is filtered
+			 */
+			const date1 = new Date();
+			return data.filter((obj) => date1 >= new Date(obj.launch_at));
+		};
 
-<<<<<<< HEAD
+		const filterUpcomming = (data) => {
+			const date1 = new Date();
+			return data.filter((obj) => date1 < new Date(obj.launch_at));
+		};
+
+		const groupByDate = (data) => {
+			/**
+			 * Groups the object data in array
+			 * as by same date in day
+			 */
+
+			let groupedData = [];
+			let temp = [];
+			data.map((product, index) => {
+				//Checking the date of previous produdct and current product
+				// substring first 10 string because it contains the date
+				// we subtract the time
+				if (
+					temp.length != 0 &&
+					String(temp[temp.length - 1].created_at).substring(0, 10) ==
+						String(product.created_at).substring(0, 10)
+				) {
+					temp.push(product);
+				} else if (
+					temp.length != 0 &&
+					String(temp[temp.length - 1].created_at).substring(0, 10) !=
+						String(product.created_at).substring(0, 10)
+				) {
+					groupedData.push(temp);
+					temp = [];
+				}
+
+				if (temp.length == 0) {
+					temp.push(product);
+				}
+
+				if (data.length - 1 == index && temp.length != 0) {
+					// The data is last we should manually push in group data
+					groupedData.push(temp);
+				}
+			});
+			return groupedData;
+		};
+
+		if (data != undefined && data.length != 0) {
+			launchedData = groupByDate(filterLaunched(data));
+			upcommingData = filterUpcomming(data);
+		}
+
+		console.log("upcomming " + upcommingData);
+
 		return (
 			<>
 				{loginForm && !isAuthenticated ? (
@@ -137,107 +125,6 @@ const MainContent: React.FC = React.memo(
 								className="w-16 h-16 rounded-full"
 							/>
 						</div>
-=======
-	//Pop us when click to profile image if loged in is false
-	const [loginForm, toggle] = useToggle(false);
-	const [productOrderBy, setProductOrderBy] = useState(NEWEST)
-	const router = useRouter();
-	let launchedData , upcommingData;
-
-	const { data, error, isLoading, isError } = useQuery("products", fecthProducts);
-
-	if (isError || error )
-	{
-		return <h1>Error occurs </h1>;
-	}
-
-	if (isLoading)
-	{
-		return <h1>Data is loading </h1>;
-	}
-
-
-	const filterLaunched = (data) => {
-		/**
-		 * This function filters the data according to the 
-		 * Date it is launched . Only already lauhced data is filtered
-		 */
-		const date1 = new Date();
-		return data.filter(obj => date1 >= new Date(obj.launch_at))
-	}
-
-	const filterUpcomming = (data) => {
-		const date1 = new Date()
-		return data.filter(obj => date1 < new Date(obj.launch_at))
-
-	}
-
-
-	const groupByDate = (data) => {
-		/**
-		 * Groups the object data in array 
-		 * as by same date in day 
-		 */
-
-		let groupedData = []
-		let temp = []
-		data.map((product, index) => {
-			//Checking the date of previous produdct and current product 
-			// substring first 10 string because it contains the date 
-			// we subtract the time 
-			if (temp.length != 0 && String(temp[temp.length - 1].created_at).substring(0,10) == String(product.created_at).substring(0,10)){
-				temp.push(product)
-			} else if(temp.length != 0 && String(temp[temp.length - 1].created_at).substring(0,10) != String(product.created_at).substring(0,10)){
-				groupedData.push(temp)
-				temp = [];
-			}
-
-			if (temp.length == 0)
-			{
-				temp.push(product)
-			}
-
-			if (data.length - 1 == index && temp.length != 0)
-			{
-				// The data is last we should manually push in group data
-				groupedData.push(temp)
-			}
-
-			
-		})
-		return groupedData;
-	}
-
-	if (data != undefined && data.length != 0)
-	{
-		 launchedData= groupByDate(filterLaunched(data));
-		 upcommingData = filterUpcomming(data);
-	}
-
-console.log("upcomming " + upcommingData)
-
-
-
-
-	return (
-		<>
-		
-			{ loginForm && !isAuthenticated ? (
-				<ToggleContext.Provider value={toggle}>
-					<AuthForm/>
-				</ToggleContext.Provider>
-			) : null}
-
-
-			<div className="h-auto w-full rounded-t-lg -mt-4 bg-drak_blue_background z-10">
-				<div className="w-16 h-16 rounded-full m-auto relative -mt-8 mb-4">
-					<div className="profile-image-back w-16 h-16 rounded-full absolute"></div>
-					<div className="absolute cursor-pointer" onClick={toggle}>
-						<img
-							src={ user != null ? user.profile_image : "images/michaeljackson.jpg"}
-							className="w-16 h-16 rounded-full"
-						/>
->>>>>>> e39231f6c72a3e90b97062008bea6fc0b729ac5d
 					</div>
 					<IconContext.Provider value={{ color: "#ffffff", size: "1.5rem" }}>
 						<div>
@@ -282,30 +169,14 @@ console.log("upcomming " + upcommingData)
 									Newest
 								</button>
 							</div>
-							{initialData.map((cardData) => {
+							{launchedData.map((cardData) => {
 								return <ProductListCard data={cardData} key={uuidv4()} />;
 							})}
 						</div>
 						<div className="right-container h-auto pt-1 mr-4 lg:mr-40">
-							<UpcomingProductCard key={uuidv4()} />
+							{<UpcomingProductCard data={upcommingData} key={uuidv4()} />}
 							<NewsLetterCard key={uuidv4()} />
 						</div>
-<<<<<<< HEAD
-=======
-						{
-							launchedData.map(cardData => {
-								return <ProductListCard data={cardData} key= {uuidv4()}/>
-							})
-						}
-
-					</div>
-					<div className="right-container h-auto pt-1 mr-4 lg:mr-40">
-						{
-							
-							< UpcomingProductCard data={upcommingData} key={uuidv4()} />
-						}
-						<NewsLetterCard key={uuidv4()}/>
->>>>>>> e39231f6c72a3e90b97062008bea6fc0b729ac5d
 					</div>
 				</div>
 
@@ -328,7 +199,6 @@ console.log("upcomming " + upcommingData)
 							-moz-animation: spin 4s linear infinite;
 							animation: spin 4s linear infinite;
 						}
-
 						.option-btn {
 							background: conic-gradient(
 								from 180deg at 50% 50%,
@@ -343,37 +213,30 @@ console.log("upcomming " + upcommingData)
 							);
 							filter: blur(2px);
 						}
-
 						.btn {
 							border: none;
 							outline: none;
 						}
-
 						.line {
 							width: 3px;
 							height: 100%;
 							background: linear-gradient(#2f80ed, #ec1616);
 						}
-
 						.left-container {
 							width: 100%;
 						}
-
 						.right-container {
 							display: none;
 						}
-
 						@media only screen and (min-width: 768px) {
 							.left-container {
 								width: 65%;
 							}
-
 							.right-container {
 								width: 35%;
 								display: block;
 							}
 						}
-
 						// Animation for 360 rotation background
 						@-moz-keyframes spin {
 							100% {
