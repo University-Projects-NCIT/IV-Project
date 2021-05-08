5622391118f08f10766d0fe990545e3873d734fb
karkigrishmin <grishmink@gmail.com>
2021-05-08 15:21:47 +0545

    minor changes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -90,8 +90,8 @@ DATABASES = {
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = ''
-EMAIL_HOST_PASSWORD = ''
+EMAIL_HOST_USER = 'astartup02@gmail.com'
+EMAIL_HOST_PASSWORD = 'startupa@2020'
 EMAIL_USE_TLS = True
 
 
