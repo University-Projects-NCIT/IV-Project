d3a8559e9e89c54fc89720e203407d52bcacadb8
unknown <rupachasgvn@gmail.com>
2021-05-14 10:42:17 +0545

    main back image opacity decreased

--- frontend/core/src/components/MainComponent.tsx
+++ frontend/core/src/components/MainComponent.tsx
@@ -8,7 +8,7 @@ const MainComponent: React.FC = () => {
 		<>
 			<div className="w-full h-auto bg-white-500 flex flex-col">
 				<div className="w-full h-32 sm:h-60">
-					<div className="w-full h-32 sm:h-60 z-10 bg-black opacity-30 fixed"></div>
+					<div className="w-full h-32 sm:h-60 z-10 bg-black opacity-20 fixed"></div>
 					<div className="header w-full h-32 sm:h-60 fixed z-0">
 						<img src="./images/startup.jpg" className="object-cover w-full h-full"></img>
 					</div>
