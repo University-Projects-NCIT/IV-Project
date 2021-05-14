b7c03b977bb0bb2bd1e7de50329afff121b6dc7a
unknown <rupachasgvn@gmail.com>
2021-05-14 11:27:08 +0545

    google auth added about to deploy vercel

--- frontend/core/src/constraints.ts
+++ frontend/core/src/constraints.ts
@@ -1,5 +1,5 @@
-// export const BACKEND_URL = 'https://startup-hunt.herokuapp.com'
-export const BACKEND_URL = 'http://localhost:9000'
+export const BACKEND_URL = 'https://startup-hunt.herokuapp.com'
+// export const BACKEND_URL = 'http://localhost:9000'
 export const LAUNCHED = "launched"
 export const UPCOMMING = "upcomming"
 export const NEWEST = "created_at"
