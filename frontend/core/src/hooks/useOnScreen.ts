88062dab259cbe6ff609e49485cbb8bbcb7e34ad
unknown <rupachasgvn@gmail.com>
2021-09-08 12:31:24 +0545

    fixing the pagination problem with window intersection observer on useOnScreen.ts

--- frontend/core/src/hooks/useOnScreen.ts
+++ frontend/core/src/hooks/useOnScreen.ts
@@ -1,32 +1,57 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
-
-
-export const useOnScreen = () => {
+ 
+export const useOnScreen = (options, ref) => {
   // State and setter for storing whether element is visible
 	const [isIntersecting, setIntersecting] = useState(false);
-  const [ref, setRef] = useState(null)
-  
-  const rootMargin = "0px"
+
+  // console.log("usescreen called ")
+  console.log(ref)
+
+  const callbackFunction = (entries) => {
+    const [entry] = entries
+    setIntersecting(entry.isIntersecting)
+    console.log("entry called " , entry.isIntersecting)
+  }
+
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        // Update our state when observer callback fires
-        setIntersecting(entry.isIntersecting);
-      },
-      {
-        rootMargin,
-      }
-    );
-    if (ref) {
-      observer.observe(ref);
+
+    if (ref == null) return
+    
+    const observer = new IntersectionObserver(callbackFunction, options)
+    if (ref.current) observer.observe(ref.current)
+    
+    return () => {
+      if(ref.current) observer.unobserve(ref.current)
     }
-		return () => {
-			if (ref)
-			{
-				observer.unobserve(ref);
-			}
-    };
-  }, [ref]); // Empty array ensures that effect is only run on mount and unmount
-  return [setRef, isIntersecting];
+  }, [ref,options])
+  
+
+  // const rootMargin = "0px"
+  // console.log("usescreen called ")
+
+
+  // useEffect(() => {
+  //   const observer = new IntersectionObserver(
+  //     ([entry]) => {
+  //       // Update our state when observer callback fires
+  //       setIntersecting(entry.isIntersecting);
+  //     },
+  //     {
+  //       rootMargin,
+  //     }
+  //   );
+  //   if (ref) {
+  //     observer.observe(ref);
+  //   }
+	// 	return () => {
+	// 		if (ref)
+	// 		{
+	// 			observer.unobserve(ref);
+	// 		}
+  //   };
+  // }, [ref]); // Empty array ensures that effect is only run on mount and unmount
+  
+  
+  return [ref, isIntersecting];
 }
