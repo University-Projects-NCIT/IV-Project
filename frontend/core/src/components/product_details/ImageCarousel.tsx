f223433f1aca7c820e555632c1c14f1297db7e12
Karkigrishmin <grishmink@gmail.com>
2021-09-10 15:41:02 +0545

    add product images in the details page

--- frontend/core/src/components/product_details/ImageCarousel.tsx
+++ frontend/core/src/components/product_details/ImageCarousel.tsx
@@ -1,76 +1,66 @@
 import React, { useState, useEffect } from "react";
 import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from "react-icons/fa";
-import { ProductImagesInteface } from "../../interfaces/Interfaces";
+import { ProductImagesProps } from "../../interfaces/Interfaces";
 
-const ImageCarousel: React.FC = () => {
+const ImageCarousel: React.FC<ProductImagesProps> = ({
+	product_images,
+}: ProductImagesProps) => {
 	let defaultData = [
 		{
-			img: "./images/default_image.png",
-		},
-	];
-
-	let api = [
-		{
-			img: "./images/snap1.jpeg",
-		},
-		{
-			img: "./images/snap2.jpeg",
-		},
-		{
-			img: "./images/snap3.jpeg",
+			image: "./images/default_image.png",
 		},
 	];
 
 	const [data, setdata] = useState(defaultData);
 	const [current, setCurrent] = useState(0);
-	const [imgUrl, setImgUrl] = useState(data[current].img);
+	const [imgUrl, setImgUrl] = useState(data[current].image);
 
 	const nextImage = () => {
 		const index = (current + 1) % data.length;
 		setCurrent(index);
-		setImgUrl(data[index].img);
+		setImgUrl(data[index].image);
 	};
 
 	const previousImage = () => {
 		const index = current == 0 ? data.length - 1 : (current - 1) % data.length;
 		console.log("prev " + index);
 		setCurrent(index);
-		setImgUrl(data[index].img);
+		setImgUrl(data[index].image);
 	};
 
 	useEffect(() => {
 		// Checking if api response if giving not
 		// Empty data or not , sets default if empty
-		if (api.length !== 0) {
-			setdata(api);
+		if (product_images.length !== 0) {
+			setdata(product_images);
 
 			// Display the first image to image board
-			setImgUrl(api[0].img);
+			setImgUrl(product_images[0].image);
 		}
 	}, []);
 
 	return (
 		<>
-			<div className="w-full flex space-between items-center">
-				<div className="text-3xl z-10 cursor-pointer" onClick={previousImage}>
+			<div className='w-full flex space-between items-center'>
+				<div className='text-3xl z-10 cursor-pointer' onClick={previousImage}>
 					<FaArrowAltCircleLeft />
 				</div>
-				<div className="w-full h-96 p-16  overflow-hidden flex items-center">
-					<img src={imgUrl} width="100%;" className="active" />
+				<div className='w-full h-96 p-16  overflow-hidden flex items-center'>
+					<img src={imgUrl} width='100%;' className='active' />
 				</div>
 
-				<div className="text-3xl z-10 cursor-pointer" onClick={nextImage}>
+				<div className='text-3xl z-10 cursor-pointer' onClick={nextImage}>
 					<FaArrowAltCircleRight />
 				</div>
 			</div>
-			<div className=" h-14 mt-6 overflow-hidden w-full flex flex-row items-center">
+			<div className=' h-14 mt-6 overflow-hidden w-full flex flex-row items-center'>
 				{data.map((img, i) => {
 					return (
 						<div key={i}>
 							<img
-								src={img.img}
-								className="w-28"
-								onClick={() => setImgUrl(img.img)}
+								src={img.image}
+								className='w-28'
+								onClick={() => setImgUrl(img.image)}
 							></img>
 						</div>
 					);
--- frontend/core/src/components/product_details/ProductDescription.tsx
+++ frontend/core/src/components/product_details/ProductDescription.tsx
@@ -6,29 +6,27 @@ import DetailPageButtons from "./DetailPageButtons";
 import ImageCarousel from "./ImageCarousel";
 import ProductHunter from "./ProductHunter";
 
-const ProductDescription: React.FC<ProductInterface> = (props) => {
-	// const initialValue = {
-	// 	title: "Instagram App",
-	// 	tagline: "The way to share your photo to the world .",
-	// 	category: ["IOS", "Android", "Website"],
-	// 	img: "./images/snapchat.png",
-	// };
-	let { product_images, description } = props;
-
+const ProductDescription: React.FC<ProductInterface> = ({
+	product_images,
+	description,
+	author,
+}: ProductInterface) => {
 	let userID;
 
-	if (typeof props != "undefined") {
-		userID = props.author;
+	if (typeof author !== "undefined") {
+		userID = author;
 	}
 
 	const fetchUser = useQuery(["user", userID], fetchUserByID);
 
-	console.log("fetch user", fetchUser.data);
+	if (typeof fetchUser !== "undefined") {
+		console.log("fetch user", fetchUser);
+	}
 
 	return (
-		<div className="mt-8 flex items-start">
+		<div className='mt-8 flex items-start'>
 			{/* product description container */}
-			<div className="bg-item_list_bg rounded-sm p-6 w-3/5">
+			<div className='bg-item_list_bg rounded-sm p-6'>
 				{/* screenshots of product */}
 				<div>
 					{/* {typeof product_images != "undefined"
@@ -36,19 +34,17 @@ const ProductDescription: React.FC<ProductInterface> = (props) => {
 								return <ImageCarousel image={image} />;
 						  })
 						: null} */}
-					<ImageCarousel />
+					<ImageCarousel product_images={product_images} />
 				</div>
 
 				{/* product description */}
-				<div className="mt-5">
-					<p className="text-sm text-gray-300">
-						{props.description}
-					</p>
+				<div className='mt-5'>
+					<p className='text-sm text-gray-300'>{description}</p>
 				</div>
 			</div>
 
 			{/* right side container */}
-			<div className=" w-auto ml-10 space-y-7">
+			<div className=' w-auto ml-10 space-y-7'>
 				<DetailPageButtons />
 				{typeof fetchUser.data != "undefined" && (
 					<ProductHunter {...fetchUser.data} />
